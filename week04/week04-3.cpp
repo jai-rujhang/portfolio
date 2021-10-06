047c374d3fef348e6f8daae2769d6ad9c5f51805
JAI-RU-JHANG <91170706+jai-rujhang@users.noreply.github.com>
2021-10-06 11:58:13 +0800

    Add files via upload

---
+++ week04/week04-3.cpp
@@ -0,0 +1,12 @@
+# include <stdio.h>
+int main ()
+{
+    float pi=3.1415926535879;
+    double pii=3.1415926535879;
+    printf("%f\n",pi);
+    printf("%f\n",pii);
+
+    printf("%.8f\n",pii);
+    printf("%.9f\n",pii);
+    printf("%9.5f\n",pii);
+}
